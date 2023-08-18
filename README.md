16ce868edc2969f39331142bf0e6a5ddbe7482b8
alvinbengeorge <alvinallen333@gmail.com>
2023-08-18 15:20:16 +0530

    ✨ Adding basic backend

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ctrl-alt-del
 Project for Quantathon
+
+# `Installation`
+
+```sh
+pip3 install -r requirements.txt
+
+# Deployment
+uvicorn main:app
+
+# Development
+uvicorn main:app --reload
+
+```
---
+++ main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+from qiskit import Aer
+from qiskit.algorithms.optimizers import SPSA
+from qiskit.algorithms import VQE
+from qiskit.opflow import I, Z, X, Y, PauliSumOp
+from qiskit.circuit.library import TwoLocal
+from qiskit.utils import QuantumInstance
+from fastapi.encoders import jsonable_encoder
+from matplotlib import pyplot as plt
+
+app = FastAPI()
+
+from typing import List
+from pydantic import BaseModel
+
+
+class PauliTerm(BaseModel):
+    coefficient: float
+    operator: str
+
+
+class HamiltonianInput(BaseModel):
+    terms: List[PauliTerm]
+
+def plot(values):
+    plt.plot(values)
+    plt.show()
+
+
+
+@app.post("/balls")
+def calculate_eigenvalues(hamiltonian: HamiltonianInput, iterations: int = 5):
+    pauli_terms = []
+    for term in hamiltonian.terms:
+        pauli_term = term.coefficient * I
+        for char in term.operator:
+            if char == "I":
+                continue
+            if char == "X":
+                pauli_term = pauli_term ^ X
+            elif char == "Y":
+                pauli_term = pauli_term ^ Y
+            elif char == "Z":
+                pauli_term = pauli_term ^ Z
+            else:
+                raise ValueError("Invalid Pauli operator:", char)
+        pauli_terms.append(pauli_term)
+    
+    print([str(i) for i in pauli_terms])
+    hamiltonian_op = sum(pauli_terms)
+
+    num_assets = len(hamiltonian.terms[0].operator)
+    ansatz = TwoLocal(num_assets, "ry", "cz", reps=3, entanglement="full")
+    print(ansatz)
+
+    backend = Aer.get_backend("aer_simulator")
+
+    optimizer = SPSA(maxiter=100)
+    vqe = VQE(
+        ansatz=ansatz,
+        optimizer=optimizer,
+        quantum_instance=QuantumInstance(backend=backend),
+    )
+
+    results = []
+    for _ in range(iterations):
+        result = vqe.compute_minimum_eigenvalue(hamiltonian_op)
+        eigenstate = str(result.eigenstate)
+        eigenvalue = str(result.eigenvalue)
+        var_params = list(result.optimal_parameters.values())
+        print("Eigenstate:", result.optimal_parameters)
+        iteration_result = {
+         "eigenstate": eigenstate,
+         "eigenvalue": eigenvalue,
+         "variational_parameters": var_params,
+        }
+        results.append(iteration_result)
+    
+    # plot(var_params)
+
+    return jsonable_encoder(results)
